7e2597ebfcd6d4a6b63861f383754f3ea6ea4ff0
Mukund Singh Bisht <Mukundsingh05@outlook.com>
2025-07-27 15:04:24 +0530

    Activity screen V3

--- practice-app/app/_layout.tsx
+++ practice-app/app/_layout.tsx
@@ -3,8 +3,12 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
+import { LogBox } from "react-native";
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+LogBox.ignoreLogs([
+  "VirtualizedLists should never be nested", // Ignore specific warning
+]);
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
